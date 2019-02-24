ff49c1aa852b692c1918ddf1696d2740b528d9cc
Ravneet Kaur <47275109+ravneetKaur2159@users.noreply.github.com>
2019-02-25 01:25:47 +0800

    LR completion

--- SentimentAnalysis/DataPreprocessing.py
+++ SentimentAnalysis/DataPreprocessing.py
@@ -1,11 +1,12 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import DataCleaning
+import numpy
 
 def getVectorizedData():
     cleanedData = DataCleaning.getPreProcessedData()
     vectorizedData = {}
+
     #vectorization - one-hot encoding
     vectorizer = CountVectorizer(binary=True)
-    vectorizedData["vectorizedPositiveData"] = vectorizer.fit_transform(cleanedData["trainedPositiveReview"])
-    vectorizedData["vectorizedDataNegative"] = vectorizer.fit_transform(cleanedData["trainedNegativeReview"])
+    vectorizedData = vectorizer.fit_transform(cleanedData)
     return vectorizedData
